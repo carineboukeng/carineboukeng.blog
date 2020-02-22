affa7bf7e6291d84913c788e64b7303c6f5859ee
carineboukeng <carinesainclair.ANAGMOBOUKENG@student.umons.ac.be>
2020-02-22 13:16:47 +0100

    tytt

--- _posts/changing article.R
+++ _posts/changing article.R
@@ -1,2 +1,22 @@
-distill::create_post("article-6")
+---
+title: "article5:Les lichens, de suprenants organismes pioniers"
+description: |
+  A short description of the post.
+author:
+  - name: Anagmo boukeng carine sainclair
+date: 02-15-2020
+output:
+  distill::distill_article:
+    self_contained: false
+---
+
+```{r setup, include=FALSE}
+knitr::opts_chunk$set(echo = FALSE)
+```
+
+Distill is a publication format for scientific and technical writing, native to the web.
+
+Learn more about using Distill at <https://rstudio.github.io/distill>.
+
+![](https://www.google.com/search?q=environnement&rlz=1C1CHBF_frBE886BE887&sxsrf=ALeKk00deCiKeI2LS6ZlrcS5kxVzm4IMtA:1582358586149&source=lnms&tbm=isch&sa=X&ved=2ahUKEwie2LyL2eTnAhUFSxUIHYx)
 
